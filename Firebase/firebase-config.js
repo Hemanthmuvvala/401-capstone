0766ac4db59e46df055cae6bd90443292c22b95c
hemanth venkata naga pavan kumar <160225341+Hemanthmuvvala@users.noreply.github.com>
2025-04-03 22:51:05 +0530

    Update firebase-config.js

--- Firebase/firebase-config.js
+++ Firebase/firebase-config.js
@@ -1,9 +1,9 @@
-// Import Firebase SDK
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
 
-// Your Firebase configuration
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyAM62Q4zNrVAELRqBxUfOvXLi6z-b7oF3k",
@@ -15,7 +15,7 @@ const firebaseConfig = {
   
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
